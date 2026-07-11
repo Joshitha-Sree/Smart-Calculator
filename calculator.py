46ad59351164f90acff424dfc10e101a9c673ec0
P Joshitha Sree <joshithasree.edu@gmail.com>
2026-07-12 03:10:17 +0530

    Create calculator welcome screen and menu

---
+++ calculator.py
@@ -0,0 +1,20 @@
+"""
+Project: Smart Calculator
+Author: Josh
+Version: 1.0
+Description:
+A command-line calculator built using Python.
+This project is part of my GitHub Engineering Portfolio.
+"""
+print("=" * 50)
+print("        SMART CALCULATOR")
+print("=" * 50)
+print("Welcome!")
+print()
+print("Choose an operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+print("5. Exit")
+choice = input("Enter your choice (1-5): ")
